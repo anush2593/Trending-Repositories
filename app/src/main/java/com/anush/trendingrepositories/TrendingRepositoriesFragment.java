5b4e96b61f9e938905275ffab583141f436ae8db
anush2593 <anush.hakobyan20@gmail.com>
2023-01-01 13:27:07 +0400

    Added navigation component Created TrendingRepositoriesFragment ui

---
+++ app/src/main/java/com/anush/trendingrepositories/TrendingRepositoriesFragment.java
@@ -0,0 +1,41 @@
+package com.anush.trendingrepositories;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.anush.trendingrepositories.databinding.FragmentTrendingRepositoriesBinding;
+
+
+public class TrendingRepositoriesFragment extends Fragment {
+
+    private FragmentTrendingRepositoriesBinding binding;
+
+    public TrendingRepositoriesFragment() {
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        binding = FragmentTrendingRepositoriesBinding.inflate(inflater, container, false);
+        View view = binding.getRoot();
+        return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
+}
